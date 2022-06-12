3dce03ad0a47283edd7a81ac607ec72efbeac20e
Mihai Turcu <mihaiturcu@Mihais-Mac-mini-2.local>
2022-06-13 00:58:03 +0300

    2017 school data

---
+++ 2017/static/js/engine_gender.js
@@ -0,0 +1,200 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function add_css(filename){
+  var base_static = 'https://mihaitc.github.io/scoli/static/';
+  var head  = document.getElementsByTagName('head')[0];
+  var link  = document.createElement('link');
+  link.rel  = 'stylesheet';
+  link.type = 'text/css';
+  link.href = base_static + 'css/' + filename;
+  link.media = 'all';
+  head.appendChild(link);  
+}
+
+add_css('leaflet.css');
+add_css('scoli.css');
+add_css('bootstrap.min.css');
+
+var female_draw_color = "red";
+var male_draw_color = "blue";
+var no_color = "";
+
+function initialize() {
+  var hash_values = read_hash();
+  if(hash_values.hasOwnProperty('lt') && !isNaN(hash_values.lt)){
+    var map_latitude = hash_values.lt;
+  }  
+  else {
+    var map_latitude = json['pn_geo']['lt'];
+  }
+  if(hash_values.hasOwnProperty('lg') && !isNaN(hash_values.lg)){
+    var map_longitude = hash_values.lg;
+  }  
+  else {
+    var map_longitude = json['pn_geo']['lg'];
+  }
+  if(hash_values.hasOwnProperty('z') && !isNaN(hash_values.z)){
+    map_zoom = hash_values.z;
+  }  
+  else{
+    var map_zoom = json['pn_geo']['z'];
+  }
+  
+  // var streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWloYWl0YyIsImEiOiJQdlZ3Vk1jIn0.tifaZEFZJjYcbkOBRooqGw', {
+  var streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}-v11/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWloYWl0YyIsImEiOiJQdlZ3Vk1jIn0.tifaZEFZJjYcbkOBRooqGw', {
+		maxZoom: 18,
+		id: 'streets'
+	});
+	var here_hybrid = L.tileLayer.provider('HERE.hybridDay', {
+      app_id: 'e8cITM4ZmLZY5vCISfGG',
+      app_code: 'eWDxP5uk72zORpQjf8GANw'
+  });
+  var group_data = {
+    all_grades: new L.FeatureGroup(),
+    female_grades: new L.FeatureGroup(),
+    male_grades: new L.FeatureGroup(),
+  };
+  var selected_grades = group_data.all_grades; 
+  if(hash_values.hasOwnProperty('av')){
+    if(hash_values['av'] == 'a'){
+      selected_grades = group_data.female_grades;
+    }
+    else if(hash_values['av'] == 'b'){
+      selected_grades = group_data.male_grades;
+    }    
+  };      
+  
+  map = L.map('map', {
+      center: [map_latitude, map_longitude],
+      zoom: map_zoom,
+      layers: [streets, selected_grades]
+  });
+   
+  map.on('overlayadd', function(e) {
+    if(e.name == 'Băieți'){
+      update_avg('b');
+    }
+    else if(e.name == 'Fete'){
+      update_avg('a');      
+    }
+    else if(e.name == 'Toate'){
+      update_avg('');            
+    }    
+  });  
+  
+  var marker_options = {
+      weight: 1,
+      opacity: 1
+  };
+
+  var InfoControl = L.Control.extend({
+      options: {
+          position: 'bottomleft'
+      },
+
+      onAdd: function (map) {
+          // create the control container with a particular class name
+          var average_diff_color = json['males_a'] ? male_draw_color : female_draw_color;
+          var container = L.DomUtil.create('div', 'info-control');
+          var control_html = '<b>Școlile din '+ json['place_name'];
+          control_html += ' după media notelor</b><br />';
+          control_html += '<span style="color:' + female_draw_color + '">Media fete ';
+          if(json['class_name']){
+            control_html += '(' + json['class_name']  + ') ';
+          }
+          control_html += 'pe ' + json['place_type'] + ': ' + json['average_f'] + '</span><br />';
+          control_html += '<span style="color:' + male_draw_color + '">Media băieți ';
+          if(json['class_name']){
+            control_html += '(' + json['class_name']  + ') ';
+          }
+          control_html += 'pe ' + json['place_type'] + ': ' + json['average_m'] + '</span><br />';
+          control_html += 'Diferență: <span style="color:' + average_diff_color + '">' + json['average_diff'] + '</span><br />';
+          control_html += 'Fete: ' + numberWithCommas(json['female_count']) + ', băieți: ' +  numberWithCommas(json['male_count']);
+          container.innerHTML = control_html;
+
+          L.DomEvent.disableClickPropagation(container);
+          return container;
+      }
+  });
+  map.addControl(new InfoControl());  
+  
+  // Overlay layers are grouped
+  var groupedOverlays = {
+    "Note": {
+      "Toate": group_data.all_grades,
+      "Fete": group_data.female_grades,
+      "Băieți": group_data.male_grades,
+    },
+  };
+
+  var group_options = {
+    // Make the "Note" group exclusive (use radio inputs)
+    exclusiveGroups: ["Note", ],
+  };
+  var base_maps = {"Hibrid": here_hybrid, "Strazi": streets, }
+  L.control.groupedLayers(base_maps, groupedOverlays, group_options).addTo(map);
+  var placename_selector = L.control.placenameSelect({position: 'bottomright'});
+  placename_selector.addTo(map);
+  placename_selector.on('change', function(e){});
+  var class_selector = L.control.classSelect({position: 'bottomright'});
+  class_selector.addTo(map);
+  class_selector.on('change', function(e){});
+  if(json['school_type'] != 'oraș'){
+    var school_type_selector = L.control.schoolTypeSelect({position: 'bottomright'});
+    school_type_selector.addTo(map);
+    school_type_selector.on('change', function(e){});
+  };    
+  
+  var year_selector = L.control.yearSelect({position: 'bottomright'});
+  year_selector.addTo(map);  
+  year_selector.on('change', function(e){});  
+  
+  var schools = json['schools'];
+  for(var i=0; i < schools.length; i++){
+    var diff_color = '';
+    if(schools[i].ba){
+      marker_options.fillColor = male_draw_color;
+      marker_options.color = male_draw_color;
+      diff_color = male_draw_color;
+    }
+    else {
+      marker_options.fillColor = female_draw_color;
+      marker_options.color = female_draw_color;
+      diff_color = female_draw_color;
+    }
+    marker_options.radius =  schools[i].r;
+    marker_options.fillOpacity =  schools[i].f;
+    var marker = L.circleMarker(L.latLng(schools[i].lt, schools[i].lg), marker_options);
+    var text = schools[i].sn + "<br /><span style=\"color:" + female_draw_color + "\">Nota medie fete";
+    if(json['class_name']){
+      text += ' (' + json['class_name']  + ')';
+    }
+    text += ": " + schools[i].agf + '</span>';
+    text += "<br /><span style=\"color:" + male_draw_color + "\">Nota medie băieți";
+    if(json['class_name']){
+      text += ' (' + json['class_name']  + ')';
+    }
+    text += ': ' + schools[i].agm + '</span>';
+    text += "<br />Diferență: <span style=\"color:" + diff_color + "\">" + schools[i].df + "</span>";
+    text += "<br/ >Număr elevi: " + schools[i].n;
+    text += " (<span style=\"color:" + no_color + ";\">f: " + schools[i].nf + "</span>, <span style=\"color:" + no_color + ";\">b: " + schools[i].nm + "</span>)";
+    marker.bindPopup(text).addTo(group_data.all_grades);
+    if(schools[i].ba){
+      L.circleMarker(L.latLng(schools[i].lt, schools[i].lg), marker_options).bindPopup(text).addTo(group_data.male_grades);
+    }
+    else {
+      L.circleMarker(L.latLng(schools[i].lt, schools[i].lg), marker_options).bindPopup(text).addTo(group_data.female_grades);
+    }
+  };
+  if(json['bounds']['res_longitude_sw'] !== null && json['bounds']['res_longitude_ne'] !== null){
+    var hash_values = read_hash();
+    if(!hash_values.hasOwnProperty('lt') || (hash_values.hasOwnProperty('lt') && isNaN(hash_values.lt))){
+      map.fitBounds(group_data.all_grades.getBounds());
+    }
+  }
+  map.on('load moveend', function(event){
+    map_move_end(map);
+  });  
+}
---
+++ 2017/static/js/leaflet.gender_custom_controls.js
@@ -0,0 +1,362 @@
+var class_names = ['romana', 'mate', ]
+var school_type_names = ['rural', 'urban']
+
+L.Control.SchoolTypeSelect = L.Control.extend({
+ onAdd: function(map) {
+   var school_type = '';
+   var new_path_split = new Array();
+   var path_split = window.location.pathname.split('/');
+   for(var i=0; i < path_split.length; i++){
+     if(path_split[i]){
+       new_path_split.push(path_split[i]);
+     }
+   };
+   if(new_path_split.length == 2){
+     new_path_split.push('bucuresti');
+   }
+   if(new_path_split){
+     var place_name = new_path_split[new_path_split.length-1];
+     var place_name_split = place_name.split('_');
+     if(place_name_split){
+       var intersection = intersect(place_name_split, school_type_names);
+       if(intersection){
+         var school_type = intersection[0];
+       }       
+     }
+   }
+   // console.log(place_name);
+   var school_type_dict = {};
+   for(var i=0; i < school_type_names.length; i++){
+     if(school_type_names[i] == school_type){
+       school_type_dict[school_type_names[i]] = 'selected="SELECTED"';
+     }
+     else {
+       school_type_dict[school_type_names[i]] = '';
+     }
+   }
+
+   this.div = L.DomUtil.create('div','leaflet-schooltypeselect-container');
+   this.select = L.DomUtil.create('select','leaflet-schooltypeselect',this.div);
+   this.select.onmousedown = L.DomEvent.stopPropagation;
+   var content = '';
+   content += '<option value="">Toate</option>';
+   content += '<option value="rural" ' + school_type_dict['rural'] + '>Rural</option>';
+   content += '<option value="urban" ' + school_type_dict['urban'] + '>Urban</option>';
+   this.select.innerHTML = content;
+
+   return this.div;
+ },
+  on: function(type,handler){
+    if (type == 'change'){
+      this.onChange = handler;
+      L.DomEvent.addListener(this.select,'change',this._onChange,this);
+    } else if (type == 'click'){ //don't need this here probably, but for convenience?
+      this.onClick = handler;
+      L.DomEvent.addListener(this.select,'click',this.onClick,this);
+    } else {
+      console.log('SchoolTypeSelect - cannot handle '+type+' events.')
+    }
+  },
+  _onChange: function(e) {
+    var combined = class_names.concat(school_type_names);
+    var selected_school_type = this.select.options[this.select.selectedIndex].value;
+    var new_path_split = new Array();
+    var path_split = window.location.pathname.split('/')
+    for(var i=0; i < path_split.length; i++){
+      if(path_split[i]){
+        new_path_split.push(path_split[i]);
+      }
+    };
+    if(new_path_split.length == 2){
+      new_path_split.push('bucuresti');
+    }    
+    if(new_path_split){
+      var place_name = new_path_split[new_path_split.length-1];
+      var place_name_split = place_name.split('_');
+      var new_place_name = place_name;
+      if(place_name_split.length){
+        var temp_array = new Array();
+        temp_array.push(substract(place_name_split, combined).join('_'));
+        if(selected_school_type){
+          temp_array.push(selected_school_type);
+        }
+        var class_intersection = intersect(place_name_split, class_names);
+        if(class_intersection.length){
+          temp_array.push(class_intersection[0]);
+        }
+        new_place_name = temp_array.join('_');
+      }
+      window.location.hash = '';
+      window.location.pathname = '/' + new_path_split.slice(0, -1).join('/') + '/' + new_place_name + '/';
+    }
+  }
+});
+
+L.control.schoolTypeSelect = function(opts) {
+ return new L.Control.SchoolTypeSelect(opts);
+};
+
+L.Control.PlacenameSelect = L.Control.extend({
+ onAdd: function(map) {
+   var placename = '';
+   var combined = class_names.concat(school_type_names);
+   var cities = new Array();
+   var counties = new Array();
+   var new_path_split = new Array();
+   var path_split = window.location.pathname.split('/');
+   for(var i=0; i < path_split.length; i++){
+     if(path_split[i]){
+       new_path_split.push(path_split[i]);
+     }
+   };
+   if(new_path_split.length == 2){
+     new_path_split.push('bucuresti');
+   }   
+   if(new_path_split){
+     var place_name = new_path_split[new_path_split.length-1];
+     var place_name_split = place_name.split('_');
+     if(place_name_split){       
+       placename = substract(place_name_split, combined).join('_');
+     }
+   }
+   var placename_dict = {};
+   for(var i=0; i < placenames.length; i++){
+     if(placenames[i]['place_type'] == 'city'){
+       cities.push(placenames[i]);
+     }
+     else if(placenames[i]['place_type'] == 'county'){
+       counties.push(placenames[i]);
+     }     
+     if(placenames[i]['slug'] == placename){
+       placename_dict[placenames[i]['slug']] = 'selected="SELECTED"';
+     }
+     else {
+       placename_dict[placenames[i]['slug']] = '';
+     }
+   }
+
+   this.div = L.DomUtil.create('div','leaflet-placenameselect-container');
+   this.select = L.DomUtil.create('select','leaflet-placenameselect',this.div);
+   this.select.onmousedown = L.DomEvent.stopPropagation;
+   var content = '';
+   content += '<option value="romania">România</option>';
+   content += '<optgroup label="Județe">';
+   for(var i=0; i< counties.length; i++){
+     content += '<option value="' + counties[i]['slug'] + '" ' + placename_dict[counties[i]['slug']] + '>' + counties[i]['place_name'] + '</option>';
+   }
+   content += '</optgroup>';
+   content += '<optgroup label="Orașe">';
+   for(var i=0; i< cities.length; i++){
+     content += '<option value="' + cities[i]['slug'] + '" ' + placename_dict[cities[i]['slug']] + '>' + cities[i]['place_name'] + '</option>';
+   }
+   content += '</optgroup>';  
+   this.select.innerHTML = content;
+
+   return this.div;
+ },
+  on: function(type,handler){
+    if (type == 'change'){
+      this.onChange = handler;
+      L.DomEvent.addListener(this.select,'change',this._onChange,this);
+    } else if (type == 'click'){ //don't need this here probably, but for convenience?
+      this.onClick = handler;
+      L.DomEvent.addListener(this.select,'click',this.onClick,this);
+    } else {
+      console.log('Placename - cannot handle '+type+' events.')
+    }
+  },
+  _onChange: function(e) {
+    var selected_placename = this.select.options[this.select.selectedIndex].value;
+    var new_path_split = new Array();
+    var path_split = window.location.pathname.split('/')
+    for(var i=0; i < path_split.length; i++){
+      if(path_split[i]){
+        new_path_split.push(path_split[i]);
+      }
+    };
+    if(new_path_split.length == 2){
+      new_path_split.push('bucuresti');
+    }    
+    var new_path = '/' + new_path_split.slice(0, -1).join('/') + '/' + selected_placename + '/';
+    window.location.hash = '';
+    window.location.pathname = new_path;
+  }
+});
+
+L.control.placenameSelect = function(opts) {
+ return new L.Control.PlacenameSelect(opts);
+};
+
+L.Control.ClassSelect = L.Control.extend({
+ onAdd: function(map) {
+   var gender = '';
+   var new_path_split = new Array();
+   var path_split = window.location.pathname.split('/');
+   for(var i=0; i < path_split.length; i++){
+     if(path_split[i]){
+       new_path_split.push(path_split[i]);
+     }
+   };
+   if(new_path_split.length == 2){
+     new_path_split.push('bucuresti');
+   }
+   if(new_path_split){
+     var place_name = new_path_split[new_path_split.length-1];
+     var place_name_split = place_name.split('_');
+     if(place_name_split){
+       var intersection = intersect(place_name_split, class_names);
+       if(intersection){
+         var class_name = intersection[0];
+       }
+     }
+   }
+   var class_name_dict = {};
+   for(var i=0; i < class_names.length; i++){
+     if(class_names[i] == class_name){
+       class_name_dict[class_names[i]] = 'selected="SELECTED"';
+     }
+     else {
+       class_name_dict[class_names[i]] = '';
+     }
+   }
+
+   this.div = L.DomUtil.create('div','leaflet-genderselect-container');
+   this.select = L.DomUtil.create('select','leaflet-genderselect',this.div);
+   this.select.onmousedown = L.DomEvent.stopPropagation;
+   var content = '';
+   content += '<option value="">Toate</option>';
+   content += '<option value="romana" ' + class_name_dict['romana'] + '>Lb. română</option>';
+   content += '<option value="mate" ' + class_name_dict['mate'] + '>Matematică</option>';
+   this.select.innerHTML = content;
+
+   return this.div;
+ },
+  on: function(type,handler){
+    if (type == 'change'){
+      this.onChange = handler;
+      L.DomEvent.addListener(this.select,'change',this._onChange,this);
+    } else if (type == 'click'){ //don't need this here probably, but for convenience?
+      this.onClick = handler;
+      L.DomEvent.addListener(this.select,'click',this.onClick,this);
+    } else {
+      console.log('GenderSelect - cannot handle '+type+' events.')
+    }
+  },
+  _onChange: function(e) {
+    var selected_class = this.select.options[this.select.selectedIndex].value;
+    var new_path_split = new Array();
+    var path_split = window.location.pathname.split('/')
+    for(var i=0; i < path_split.length; i++){
+      if(path_split[i]){
+        new_path_split.push(path_split[i]);
+      }
+    };  
+    if(new_path_split.length == 2){
+      new_path_split.push('bucuresti');
+    }    
+    if(new_path_split){
+      var place_name = new_path_split[new_path_split.length-1];
+      var place_name_split = place_name.split('_');
+      var new_place_name = place_name;
+      if(place_name_split.length == 1){
+        var old_place_name = place_name_split[0];
+      }
+      else if (place_name_split.length >=2 ){
+        if(intersect(place_name_split, class_names)){
+          var old_place_name =  substract(place_name_split, class_names).join('_');
+        }
+      }
+      else {
+        var old_place_name = place_name;
+      }
+      if(selected_class){
+        new_place_name = old_place_name + '_' + selected_class;
+      }
+      else {
+        new_place_name = old_place_name;
+      }
+      window.location.hash = '';
+      window.location.pathname = '/' + new_path_split.slice(0, -1).join('/') + '/' + new_place_name + '/';
+    }
+  }
+});
+
+L.control.classSelect = function(opts) {
+ return new L.Control.ClassSelect(opts);
+};
+
+L.Control.YearSelect = L.Control.extend({
+ onAdd: function(map) {
+   var year = '2021';
+   var allowed_years = ['2018', '2019', '2020', '2021'];
+   var path_split = window.location.pathname.split('/');
+   for(var i=0; i < path_split.length; i++){
+     if(path_split[i] && path_split[i].length==4 && !isNaN(path_split[i])){
+       if(allowed_years.includes(path_split[i])){
+         year = path_split[i];
+         break;
+       }
+     }
+   };
+   var year_dict = {};   
+   for(var i=0; i < allowed_years.length; i++){
+     if(allowed_years[i] == year){
+       year_dict[allowed_years[i]] = 'selected="SELECTED"';
+     }
+     else {
+       year_dict[allowed_years[i]] = '';
+     }
+   }
+
+   this.div = L.DomUtil.create('div','leaflet-yearselect-container');
+   this.select = L.DomUtil.create('select','leaflet-yearselect',this.div);
+   this.select.onmousedown = L.DomEvent.stopPropagation;
+   var content = '';
+   content += '<option value="2018" ' +  year_dict['2018'] + '>2018</option>';
+   content += '<option value="2019" ' +  year_dict['2019'] + '>2019</option>';
+   content += '<option value="2020" ' + year_dict['2020'] + '>2020</option>';   
+   content += '<option value="2021" ' + year_dict['2021'] + '>2021</option>';   
+   this.select.innerHTML = content;
+
+   return this.div;
+ },
+  on: function(type,handler){
+    if (type == 'change'){
+      this.onChange = handler;
+      L.DomEvent.addListener(this.select,'change',this._onChange,this);
+    } else if (type == 'click'){ //don't need this here probably, but for convenience?
+      this.onClick = handler;
+      L.DomEvent.addListener(this.select,'click',this.onClick,this);
+    } else {
+      console.log('Year - cannot handle '+type+' events.')
+    }
+  },
+  _onChange: function(e) {
+    var latest_year = '2021';
+    var selected_year = this.select.options[this.select.selectedIndex].value;
+    var new_path_split = new Array();
+    var year_path_list = new Array();
+    var path_split = window.location.pathname.split('/')
+    for(var i=0; i < path_split.length; i++){
+      if(path_split[i] && !is_valid_year(path_split[i])){
+        new_path_split.push(path_split[i]);
+      }
+    };
+    if(new_path_split.length == 2){
+      new_path_split.push('bucuresti');
+    }    
+    if(selected_year == latest_year){
+      year_path_list = new_path_split;
+    }
+    else {
+      year_path_list = year_path_list.concat(new_path_split.slice(0, 2), [selected_year], new_path_split.slice(2));
+    }
+    var new_path = year_path_list.join('/') + '/';
+    window.location.hash = '';
+    window.location.pathname = new_path;
+  }
+});
+
+L.control.yearSelect = function(opts) {
+ return new L.Control.YearSelect(opts);
+};
